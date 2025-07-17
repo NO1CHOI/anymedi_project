b122f6e67d547ae33b91631aae104b73b855ac6a
no1choi <pooh05307@gmail.com>
2025-07-17 16:05:26 +0900

    css완료

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,12 @@
+// a링크 이동 막기
+document.querySelectorAll('a').forEach(function(link) {
+    link.addEventListener('click', function(e) {
+    if (this.getAttribute('href') === '#' || this.getAttribute('href') === '') {
+        e.preventDefault();
+    }
+    });
+});
+
 
 // skill 영역 효과
 const skillBg = document.querySelectorAll('.skill_intro')
